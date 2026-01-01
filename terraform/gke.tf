81d2b93c69a44cce94af8a62f9ece1ca04626652
Varuna Priya <varunapriya93@gmail.com>
2026-01-01 21:24:58 +0530

    Add Terraform configs and Kubernetes manifests for GKE deployment

---
+++ terraform/gke.tf
@@ -0,0 +1,22 @@
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 30
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  default = "gke-cicd-project"
+}
+
+variable "region" {
+  default = "asia-south1"
+}
+
+variable "cluster_name" {
+  default = "cicd-gke-cluster"
+}
